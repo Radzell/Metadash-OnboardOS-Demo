cc248c966b1a6232c6951a1760ee766f132a1cc8
Deon Robinson <deon@appmunki.com>
2022-04-08 15:23:16 -0700

    Next.js app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,24 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import { OnboardOS, useOnboardOS } from 'onboard-os'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  const onboardos = useOnboardOS()
+
+  const onValidate = async (stepId: string, stepType: string, data: object) => {
+    onboardos.goForward()
+    return true
+  }
+
+  const onEnd = (data: object, schema: object) => {
+
+  }
+
+  const onAction = (stepId: string, stepType: string, data: Record<string, any>) => {
+
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,44 +28,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+       <OnboardOS apiKey={'9v67jDDsArXLQFT9gFTR'} register={onboardos.register} onValidate={onValidate} onEnd={onEnd} onAction={onAction} />
       </main>
 
       <footer className={styles.footer}>
@@ -61,7 +39,6 @@ const Home: NextPage = () => {
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
